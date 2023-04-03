c2f42d9d296e653c0bf2731deebee77e8363d000
StanislavAerialist <moonlight_stas@mail.ru>
2023-04-03 19:11:39 +0500

    2. HQL [#6874]

---
+++ src/main/java/ru/job4j/cars/repository/UserRepository.java
@@ -0,0 +1,117 @@
+package ru.job4j.cars.repository;
+
+import lombok.AllArgsConstructor;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import ru.job4j.cars.model.User;
+
+import java.util.List;
+import java.util.Optional;
+
+@AllArgsConstructor
+public class UserRepository {
+    private final SessionFactory sf;
+
+    /**
+     * Сохранить в базе.
+     * @param user пользователь.
+     * @return пользователь с id.
+     */
+    public User create(User user) {
+        Session session = sf.openSession();
+        try {
+            session.beginTransaction();
+            int savedId = (int) session.save(user);
+            user.setId(savedId);
+            session.getTransaction().commit();
+            session.close();
+        } catch (Exception e) {
+            session.getTransaction().rollback();
+        }
+        return user;
+    }
+
+    /**
+     * Обновить в базе пользователя.
+     * @param user пользователь.
+     */
+    public void update(User user) {
+        Session session = sf.openSession();
+        try {
+            session.beginTransaction();
+            session.update(user);
+            session.getTransaction().commit();
+            session.close();
+        } catch (Exception e) {
+            session.getTransaction().rollback();
+        }
+    }
+
+    /**
+     * Удалить пользователя по id.
+     * @param userId ID
+     */
+    public void delete(int userId) {
+        Session session = sf.openSession();
+        try {
+            session.beginTransaction();
+            session.createQuery(
+                            "DELETE Item WHERE id = :fId")
+                    .setParameter("fId", userId)
+                    .executeUpdate();
+            session.getTransaction().commit();
+        } catch (Exception e) {
+            session.getTransaction().rollback();
+        }
+    }
+
+    /**
+     * Список пользователь отсортированных по id.
+     * @return список пользователей.
+     */
+    public List<User> findAllOrderById() {
+        Session session = sf.openSession();
+        List<User> rsl = session.createQuery("from User as u order by u.id", User.class).list();
+        session.close();
+        return rsl;
+    }
+
+    /**
+     * Найти пользователя по ID
+     * @return пользователь.
+     */
+    public Optional<User> findById(int id) {
+        Session session = sf.openSession();
+        User user = session.get(User.class, id);
+        session.close();
+        return Optional.ofNullable(user);
+    }
+
+    /**
+     * Список пользователей по login LIKE %key%
+     * @param key key
+     * @return список пользователей.
+     */
+    public List<User> findByLikeLogin(String key) {
+        Session session = sf.openSession();
+        List<User> rsl = session.createQuery("from User where login like :uKey", User.class)
+                .setParameter("uKey", "%" + key + "%")
+                .getResultList();
+        session.close();
+        return rsl;
+    }
+
+    /**
+     * Найти пользователя по login.
+     * @param login login.
+     * @return Optional or user.
+     */
+    public Optional<User> findByLogin(String login) {
+        Session session = sf.openSession();
+        User user = session.createQuery("from User as u where u.login = :uLogin", User.class)
+                .setParameter("uLogin", login)
+                .uniqueResult();
+        session.close();
+        return Optional.ofNullable(user);
+    }
+}
