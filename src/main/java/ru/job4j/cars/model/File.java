2d46398b84903cad35f02a4aa04dad2b4f05fd8d
StanislavAerialist <moonlight_stas@mail.ru>
2023-04-24 19:15:55 +0500

    1. Фильтры для объявлений машин

--- src/main/java/ru/job4j/cars/model/File.java
+++ src/main/java/ru/job4j/cars/model/File.java
@@ -1,4 +1,19 @@
 package ru.job4j.cars.model;
 
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "file")
+@Data
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
 public class File {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @EqualsAndHashCode.Include
+    private int id;
+    private String name;
+    private String path;
 }
