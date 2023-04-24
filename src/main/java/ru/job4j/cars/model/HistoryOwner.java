5f0f548e45fdda49d546ea602a0e56826693a979
StanislavAerialist <moonlight_stas@mail.ru>
2023-04-24 18:54:35 +0500

    Модели и связи. Машины и владельцы [#4744]

---
+++ src/main/java/ru/job4j/cars/model/HistoryOwner.java
@@ -0,0 +1,30 @@
+package ru.job4j.cars.model;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "history_owner")
+@Data
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+public class HistoryOwner {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @EqualsAndHashCode.Include
+    private int id;
+
+    @ManyToOne
+    @JoinColumn(name = "car_id")
+    private Car car;
+
+    @ManyToOne
+    @JoinColumn(name = "owner_id")
+    private Owner owner;
+
+    private LocalDateTime startAt = LocalDateTime.now();
+    private LocalDateTime endAt;
+
+}
