a2a1706f41935cb9931402e2147dcb7cc4a53a89
StanislavAerialist <moonlight_stas@mail.ru>
2023-04-26 18:31:37 +0500

    Модели и связи. Машины и владельцы [#4744 #411640]

--- db/scripts/009_ddl_create_history_owner.sql
+++ db/scripts/009_ddl_create_history_owner.sql
@@ -2,7 +2,5 @@ create table history_owner
 (
     id                             serial primary key,
     car_id     int references car (id)       not null,
-    owner_id   int references owner (id)    not null,
-    startAt    timestamp                     not null,
-    endAt      timestamp                     not null
+    owner_id   int references owner (id)    not null
 );
